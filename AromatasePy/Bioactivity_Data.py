95414b56e36cf89a18f79a84a117e2e797d9ac7f
agustincarmu <agustincarmu99@gmail.com>
2023-11-02 00:07:05 -0300

    Acomodo codigo py

--- AromatasePy/Bioactivity_Data.py
+++ AromatasePy/Bioactivity_Data.py
@@ -12,4 +12,41 @@ res = activity.filter(target_chembl_id=selected_target).filter(standard_type="IC
 
 df = pd.DataFrame.from_dict(res)
 
-print(df.head(4))
+df.standard_type.unique()
+
+df.to_csv('aromatase_bioactivity_data.csv', index=False)
+
+df2 = df[df.standard_value.notna()]
+
+
+bioactivity_class = []
+for i in df2.standard_value:
+  if float(i) >= 10000:
+    bioactivity_class.append("inactive")
+  elif float(i) <= 1000:
+    bioactivity_class.append("active")
+  else:
+    bioactivity_class.append("intermediate")
+
+
+mol_cid = []
+for i in df2.molecule_chembl_id:
+  mol_cid.append(i)
+
+
+canonical_smiles = []
+for i in df2.canonical_smiles:
+  canonical_smiles.append(i)
+
+
+standard_value = []
+for i in df2.standard_value:
+  standard_value.append(i)
+
+
+data_tuples = list(zip(mol_cid, canonical_smiles, bioactivity_class, standard_value))
+df3 = pd.DataFrame( data_tuples,  columns=['molecule_chembl_id', 'canonical_smiles', 'bioactivity_class', 'standard_value'])
+
+
+df3.to_csv('aromatase_bioactivity_preprocessed_data.csv', index=False)
+
